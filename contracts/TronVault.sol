5944c51499994e3183a544c441658fed6a715bef
xtrone88 <prime18michael@gmail.com>
2022-03-10 12:20:40 -0600

    updated tronvault

--- contracts/TronVault.sol
+++ contracts/TronVault.sol
@@ -3,50 +3,44 @@ pragma solidity ^0.5.0;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
 
-contract TronVault {
+contract TronVault is Ownable {
     using SafeMath for uint256;
 
-    address owner;
     IERC20 USDT;
-    uint256 public userCount;
-    mapping(address => bool) public userLoaned;
+    mapping(address => bool) public userRefunded;
     mapping(address => uint256) public balances;
 
     constructor(address _USDT) public {
-        owner = msg.sender;
         USDT = IERC20(_USDT);
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner);
-        _;
+    function totalBalance() public view returns (uint256) {
+        return USDT.balanceOf(address(this));
     }
 
     function deposit(uint256 amount) public {
-        if (balances[msg.sender] == 0) {
-            userCount = userCount.add(1);
-            if (userLoaned[msg.sender] == true) {
-                userLoaned[msg.sender] = false;
-            }
-        }
-        balances[msg.sender] += amount;
+        require(amount >= 150000, "Too small");
+        require(totalBalance() + amount <= 15000000, "Can't deposit anymore");
+        balances[msg.sender] = balances[msg.sender].add(amount);
         USDT.transferFrom(msg.sender, address(this), amount);
     }
 
-    function loan(address receiver, uint256 price) public onlyOwner {
+    function refund(address receiver, uint256 price) public onlyOwner {
+        require(userRefunded[receiver] == false, "Already refunded");
         require(price <= 3, "Incorrect price");
         uint256 amount = balances[receiver].mul(uint256(10000).sub(price.mul(10000).div(3))).div(10000);
         balances[receiver] = balances[receiver].sub(amount);
-        if (userLoaned[receiver] == false) {
-            userCount = userCount.sub(1);
-            userLoaned[receiver] = true;
-        }
+        userRefunded[receiver] = true;
         USDT.transfer(receiver, amount);
     }
 
-    function withraw() public onlyOwner {
-        require(userCount == 0, "Not returned to all users");
-        USDT.transfer(owner, USDT.balanceOf(address(this)));
+    function withraw(uint256 amount) public onlyOwner {
+        USDT.transfer(owner(), amount);
+    }
+
+    function withrawAll() public onlyOwner {
+        USDT.transfer(owner(), USDT.balanceOf(address(this)));
     }
 }
--- test/tron.js
+++ test/tron.js
@@ -1,5 +1,7 @@
 const { expect } = require("chai")
 const { ethers, upgrades, waffle } = require("hardhat")
+const { expectRevert } = require("@openzeppelin/test-helpers")
+
 
 describe('TronVault contract', function () {
 
@@ -18,61 +20,84 @@ describe('TronVault contract', function () {
         usdt = await USDT.deploy()
         await usdt.deployed()
 
-        await usdt.transfer(user.address, 100)
-        await usdt.transfer(user2.address, 100)
-        await usdt.transfer(user3.address, 100)
+        await usdt.transfer(user.address, 200000)
+        await usdt.transfer(user2.address, 200000)
+        await usdt.transfer(user3.address, 200000)
 
         const Vault = await ethers.getContractFactory("TronVault")
         vault = await Vault.deploy(usdt.address)
         await vault.deployed()
     })
 
-    it('deposit', async () => {
-        await usdt.connect(user).approve(vault.address, 100)
-        await vault.connect(user).deposit(100)
-        expect(await usdt.balanceOf(vault.address)).to.equal(100)
+    it('deposit-1', async () => {
+        await usdt.connect(user).approve(vault.address, 200000)
+        await vault.connect(user).deposit(200000)
+        expect(await usdt.balanceOf(vault.address)).to.equal(200000)
     })
 
-    it('loan', async () => {
-        await vault.connect(owner).loan(user.address, 2)
-        expect(await usdt.balanceOf(user.address)).to.equal(33)
-        expect(await usdt.balanceOf(vault.address)).to.equal(67)
-
-        await vault.connect(owner).loan(user.address, 1)
-        expect(await usdt.balanceOf(user.address)).to.equal(77)
-        expect(await usdt.balanceOf(vault.address)).to.equal(23)
+    it('deposit-2', async () => {
+        await usdt.connect(user2).approve(vault.address, 200000)
+        await vault.connect(user2).deposit(200000)
+        expect(await usdt.balanceOf(vault.address)).to.equal(400000)
     })
 
-    it('deposit', async () => {
-        await usdt.connect(user2).approve(vault.address, 100)
-        await vault.connect(user2).deposit(100)
-        expect(await usdt.balanceOf(vault.address)).to.equal(123)
+    it('deposit-3', async () => {
+        await usdt.connect(user3).approve(vault.address, 200000)
+        await vault.connect(user3).deposit(200000)
+        expect(await usdt.balanceOf(vault.address)).to.equal(600000)
     })
 
-    it('loan', async () => {
-        await vault.connect(owner).loan(user2.address, 2)
-        expect(await usdt.balanceOf(user2.address)).to.equal(33)
-        expect(await usdt.balanceOf(vault.address)).to.equal(90)
-
-        await vault.connect(owner).loan(user.address, 1)
-        expect(await usdt.balanceOf(user2.address)).to.equal(33)
-        expect(await usdt.balanceOf(vault.address)).to.equal(75)
+    it('totalBalance', async () => {
+        expect(await usdt.balanceOf(vault.address)).to.equal(await vault.totalBalance())
+        expect(await vault.totalBalance()).to.equal(600000)
     })
 
-    it('deposit', async () => {
-        await usdt.connect(user3).approve(vault.address, 100)
-        await vault.connect(user3).deposit(100)
-        expect(await usdt.balanceOf(vault.address)).to.equal(175)
+    it('refund', async () => {
+        await vault.connect(owner).refund(user.address, 2)
+        expect(await usdt.balanceOf(user.address)).to.equal(66680)
+        expect(await usdt.balanceOf(vault.address)).to.equal(533320)
+
+        await vault.connect(owner).refund(user2.address, 1)
+        expect(await usdt.balanceOf(user2.address)).to.equal(133340)
+        expect(await usdt.balanceOf(vault.address)).to.equal(399980)
+
+        await vault.connect(owner).refund(user3.address, 0)
+        expect(await usdt.balanceOf(user3.address)).to.equal(200000)
+        expect(await usdt.balanceOf(vault.address)).to.equal(199980)
     })
 
-    it('loan', async () => {
-        await vault.connect(owner).loan(user3.address, 0)
-        expect(await usdt.balanceOf(user3.address)).to.equal(100)
-        expect(await usdt.balanceOf(vault.address)).to.equal(75)
+    it('balances', async () => {
+        expect(await vault.balances(user.address)).to.equal(133320)
+        expect(await vault.balances(user2.address)).to.equal(66660)
+        expect(await vault.balances(user3.address)).to.equal(0)
+    })
+
+    it('userRefunded', async () => {
+        expect(await vault.userRefunded(user.address)).to.equal(true)
+        expect(await vault.userRefunded(user2.address)).to.equal(true)
+        expect(await vault.userRefunded(user3.address)).to.equal(true)
+    })
+
+    it('deposit directly', async () => {
+        await usdt.connect(user3).transfer(vault.address, 200000)
+        expect(await usdt.balanceOf(vault.address)).to.equal(399980)
     })
 
     it('withraw', async () => {
-        await vault.connect(owner).withraw()
-        expect(await usdt.balanceOf(vault.address)).to.equal(0)
+        await vault.connect(owner).withraw(100000)
+        expect(await vault.totalBalance()).to.equal(299980)
+    })
+
+    it('withrawAll', async () => {
+        await vault.connect(owner).withrawAll()
+        expect(await vault.totalBalance()).to.equal(0)
+    })
+
+    it('refund again', async () => {
+        // await expectRevert(
+        //     vault.connect(owner).refund(user.address, 1),
+        //     "Already refunded"
+        // );
+        // await vault.connect(owner).refund(user.address, 1);
     })
 })
