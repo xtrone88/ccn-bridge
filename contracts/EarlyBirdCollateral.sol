c72fa3ccc3363a316d6571f0a599182bf99a647c
Metav <dev@computecoin.network>
2022-03-11 01:14:22 -0500

    Add deposit quota and authorized accounts - Other modifications.

--- contracts/EarlyBirdCollateral.sol
+++ contracts/EarlyBirdCollateral.sol
@@ -16,7 +16,9 @@ contract EarlyBirdCollateral is Ownable {
     mapping(address => bool) authorizedAccount;
     mapping(address => bool) public userRefunded;
     mapping(address => uint256) public balances;
-    mapping(address => uint256) public quota;
+    mapping(address => uint256) public quota;   // Here the address is Huygens address
+
+    event Deposit(address indexed huygens, uint256 indexed amount);
 
     modifier onlyAuthorized() {
         require(authorizedAccount[msg.sender], "Not authroized account");
@@ -36,21 +38,23 @@ contract EarlyBirdCollateral is Ownable {
         return USDT.balanceOf(address(this));
     }
 
-    function setQuota(address depositor, uint256 amount) public onlyAuthorized
+    function setQuota(address huygens, uint256 amount) public onlyAuthorized
     {
         uint256 decimals = 10 ** USDT.decimals();
         require(amount >= uint256(150000).mul(decimals), "The minimum deposit amount is 150000");
-        quota[depositor] = amount;
+        quota[huygens] = amount;
     }
 
-    function deposit(uint256 amount) public {
+    function deposit(address huygens, uint256 amount) public {
         uint256 decimals = 10 ** USDT.decimals();
         require(amount > 0, "Deposit amount is zero");
-        require(amount == quota[msg.sender], "Deposit amount doesn't match quota");
+        require(amount == quota[huygens], "Deposit amount doesn't match quota");
         require(totalBalance().add(amount) <= uint256(15000000).mul(decimals), "Can't deposit anymore");
         balances[msg.sender] = balances[msg.sender].add(amount);
-        quota[msg.sender] = 0;
+        quota[huygens] = 0;
         USDT.transferFrom(msg.sender, address(this), amount);
+
+        emit Deposit(huygens, amount);
     }
 
     function refund(address receiver, uint256 price) public onlyOwner {
