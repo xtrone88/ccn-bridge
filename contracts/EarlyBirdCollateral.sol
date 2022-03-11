b0a31be64d93ad542c6ec4e686c0fbb3368930f4
Metav <dev@computecoin.network>
2022-03-11 00:47:29 -0500

    Add deposit quota and authorized accounts.

--- contracts/EarlyBirdCollateral.sol
+++ contracts/EarlyBirdCollateral.sol
@@ -13,22 +13,43 @@ contract EarlyBirdCollateral is Ownable {
     using SafeMath for uint256;
 
     TetherToken USDT;
+    mapping(address => bool) authorizedAccount;
     mapping(address => bool) public userRefunded;
     mapping(address => uint256) public balances;
+    mapping(address => uint256) public quota;
+
+    modifier onlyAuthorized() {
+        require(authorizedAccount[msg.sender], "Not authroized account");
+        _;
+    }
 
     constructor(address _USDT) public {
         USDT = TetherToken(_USDT);
+    }
+
+    function setAuthorized(address account, bool result) public onlyOwner
+    {
+        authorizedAccount[account] = result;
     }
 
     function totalBalance() public view returns (uint256) {
         return USDT.balanceOf(address(this));
     }
 
+    function setQuota(address depositor, uint256 amount) public onlyAuthorized
+    {
+        uint256 decimals = 10 ** USDT.decimals();
+        require(amount >= uint256(150000).mul(decimals), "The minimum deposit amount is 150000");
+        quota[depositor] = amount;
+    }
+
     function deposit(uint256 amount) public {
         uint256 decimals = 10 ** USDT.decimals();
-        require(amount >= uint256(150000).mul(decimals), "Too small");
+        require(amount > 0, "Deposit amount is zero");
+        require(amount == quota[msg.sender], "Deposit amount doesn't match quota");
         require(totalBalance().add(amount) <= uint256(15000000).mul(decimals), "Can't deposit anymore");
         balances[msg.sender] = balances[msg.sender].add(amount);
+        quota[msg.sender] = 0;
         USDT.transferFrom(msg.sender, address(this), amount);
     }
 
