783798795cc3fb08fd6c2815792706d80478ac54
Metav <dev@computecoin.network>
2022-03-12 01:21:18 -0500

    Only specified Ethereum address can make deposit for a Huygens address.

--- contracts/EarlyBirdCollateral.sol
+++ contracts/EarlyBirdCollateral.sol
@@ -16,7 +16,9 @@ contract EarlyBirdCollateral is Ownable {
     mapping(address => bool) authorizedAccount;
     mapping(address => bool) public userRefunded;
     mapping(address => uint256) public balances;
-    mapping(address => uint256) public quota;   // Here the address is Huygens address
+
+    mapping(address => uint256) public quota;       // Here the address is Huygens address
+    mapping(address => address) public eth_address;  // Map from the Huygens address to Ethereum addess
 
     event Deposit(address indexed huygens, uint256 indexed amount);
 
@@ -38,15 +40,17 @@ contract EarlyBirdCollateral is Ownable {
         return USDT.balanceOf(address(this));
     }
 
-    function setQuota(address huygens, uint256 amount) public onlyAuthorized
+    function setQuota(address _eth_addess, address huygens, uint256 amount) public onlyAuthorized
     {
         uint256 decimals = 10 ** USDT.decimals();
         require(amount >= uint256(150000).mul(decimals), "The minimum deposit amount is 150000");
         quota[huygens] = amount;
+        eth_address[huygens] = _eth_addess;
     }
 
     function deposit(address huygens, uint256 amount) public {
         uint256 decimals = 10 ** USDT.decimals();
+        require(msg.sender == eth_address[huygens]);    // Only the specified Ethereum address can make a deposit
         require(amount > 0, "Deposit amount is zero");
         require(amount == quota[huygens], "Deposit amount doesn't match quota");
         require(totalBalance().add(amount) <= uint256(15000000).mul(decimals), "Can't deposit anymore");
